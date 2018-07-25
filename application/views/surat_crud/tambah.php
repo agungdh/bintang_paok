963913413549d1142e7fdd55d24115610b3b9359
agungdh <agungdh@live.com>
2018-07-26 00:19:23 +0700

    tambah modul surat crud

---
+++ application/views/surat_crud/tambah.php
@@ -0,0 +1,46 @@
+<div class="app-title">
+  <div>
+    <h1><i class="fa fa-edit"></i> Surat</h1>
+  </div>
+  <ul class="app-breadcrumb breadcrumb">
+    <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+    <li class="breadcrumb-item">Surat</li>
+  </ul>
+</div>
+<div class="row">
+  <div class="col-md-12">
+    <div class="tile">
+      <h3 class="tile-title">Tambah Surat</h3>
+      <div class="tile-body">
+        <form method="post" action="<?php echo base_url('surat_crud/aksi_tambah'); ?>" enctype="multipart/form-data">
+          
+          <div class="form-group">
+            <label class="control-label">No Surat</label>
+            <input class="form-control" type="text" required placeholder="Masukan No Surat" name="data[nosurat]">
+          </div>
+
+          <div class="form-group">
+            <label class="control-label">Pengirim</label>
+            <input class="form-control" type="text" required placeholder="Masukan Pengirim" name="data[pengirim]">
+          </div>
+
+          <div class="form-group">
+            <label class="control-label">Perihal</label>
+            <input class="form-control" type="text" required placeholder="Masukan Perihal" name="data[perihal]">
+          </div>
+
+          <div class="form-group">
+            <label class="control-label">Berkas</label>
+            <input class="form-control" type="file" required name="surat">
+          </div>
+
+          </div>
+          <div class="tile-footer">
+            <button id="simpan" class="btn btn-primary" type="button"><i class="fa fa-fw fa-lg fa-check-circle"></i>Simpan</button>
+            &nbsp;&nbsp;&nbsp;
+            <a class="btn btn-secondary" href="<?php echo base_url('surat_crud'); ?>"><i class="fa fa-fw fa-lg fa-times-circle"></i>Batal</a> <input type="submit" style="visibility: hidden;">
+          </div>
+        </form>
+    </div>
+  </div>
+</div>
--- application/views/template/menu_admin.php
+++ application/views/template/menu_admin.php
@@ -2,6 +2,8 @@
 
 <li><a class="app-menu__item" href="<?php echo base_url('bidang'); ?>"><i class="app-menu__icon fa fa-book"></i><span class="app-menu__label">Bidang</span></a></li>
 
+<li><a class="app-menu__item" href="<?php echo base_url('surat_crud'); ?>"><i class="app-menu__icon fa fa-book"></i><span class="app-menu__label">Surat</span></a></li>
+
 <li><a class="app-menu__item" href="<?php echo base_url('user'); ?>"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">User</span></a></li>
 
 <li><a class="app-menu__item" href="<?php echo base_url('config'); ?>"><i class="app-menu__icon fa fa-gears"></i><span class="app-menu__label">Config</span></a></li>
