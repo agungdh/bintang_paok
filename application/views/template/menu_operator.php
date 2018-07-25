ec37be48653b1ad2bb764126cc88828c14fa0be7
agungdh <agungdh@live.com>
2018-07-26 00:25:05 +0700

    tambah menu

---
+++ application/views/template/menu_operator.php
@@ -0,0 +1,3 @@
+<li><a class="app-menu__item" href="<?php echo base_url('dashboard'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+
+<li><a class="app-menu__item" href="<?php echo base_url('surat_crud'); ?>"><i class="app-menu__icon fa fa-book"></i><span class="app-menu__label">Surat</span></a></li>
