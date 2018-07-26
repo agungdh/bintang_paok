8508b68e1d44237dad36eac4753f502e09d4d682
agungdh <agungdh@live.com>
2018-07-26 11:36:42 +0700

    BACKUP DB

--- bintang_paok.sql
+++ bintang_paok.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 25, 2018 at 10:45 PM
+-- Generation Time: Jul 26, 2018 at 06:35 AM
 -- Server version: 10.1.29-MariaDB-6
 -- PHP Version: 5.6.36
 
@@ -41,6 +41,7 @@ INSERT INTO `bidang` (`id`, `bidang`) VALUES
 (1, 'Administrator'),
 (2, 'Sekertaris'),
 (3, 'Operator'),
+(4, 'Kepala Dinas'),
 (11, 'Perekonomian dan Sumber Daya Alam'),
 (12, 'Sosial Budaya dan Pemerintahan'),
 (13, 'Infrastruktur dan Pengembangan Wilayah'),
@@ -78,18 +79,12 @@ CREATE TABLE `disposisi` (
   `bidang_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `proses`
+-- Dumping data for table `disposisi`
 --
 
-CREATE TABLE `proses` (
-  `id` int(11) NOT NULL,
-  `surat_id` int(11) NOT NULL,
-  `waktu` datetime NOT NULL,
-  `bidang_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `disposisi` (`id`, `surat_id`, `waktu`, `keterangan`, `bidang_id`) VALUES
+(1, 9, '2018-07-26 04:18:34', 'Surat Masuk', 4);
 
 -- --------------------------------------------------------
 
@@ -104,9 +99,18 @@ CREATE TABLE `surat` (
   `pengirim` varchar(191) NOT NULL,
   `perihal` varchar(191) NOT NULL,
   `nama_file` varchar(191) NOT NULL,
+  `waktu_proses` datetime DEFAULT NULL,
+  `waktu_selesai` datetime DEFAULT NULL,
   `bidang_id` int(11) NOT NULL,
   `selesai` enum('y','t') NOT NULL DEFAULT 't'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `surat`
+--
+
+INSERT INTO `surat` (`id`, `nosurat`, `waktu`, `pengirim`, `perihal`, `nama_file`, `waktu_proses`, `waktu_selesai`, `bidang_id`, `selesai`) VALUES
+(9, '1', '2018-07-26 04:18:34', '2', '3', 'adminer-4.6.3 (1).php', NULL, NULL, 4, 't');
 
 -- --------------------------------------------------------
 
@@ -130,7 +134,8 @@ INSERT INTO `user` (`id`, `username`, `password`, `nama`, `bidang_id`) VALUES
 (1, 'admin', 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec', 'Administrator', 1),
 (2, 'sekertaris', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 'Sekertaris', 2),
 (3, 'operator', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 'Operator', 3),
-(4, 'sosmbut', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 'Sos Mbut', 12);
+(4, 'sosmbut', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 'Sos Mbut', 12),
+(5, 'kadis', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 'Kadis', 4);
 
 --
 -- Indexes for dumped tables
@@ -146,14 +151,6 @@ ALTER TABLE `bidang`
 -- Indexes for table `disposisi`
 --
 ALTER TABLE `disposisi`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `surat_id` (`surat_id`),
-  ADD KEY `bidang_id` (`bidang_id`);
-
---
--- Indexes for table `proses`
---
-ALTER TABLE `proses`
   ADD PRIMARY KEY (`id`),
   ADD KEY `surat_id` (`surat_id`),
   ADD KEY `bidang_id` (`bidang_id`);
@@ -181,31 +178,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `bidang`
 --
 ALTER TABLE `bidang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `disposisi`
 --
 ALTER TABLE `disposisi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `proses`
---
-ALTER TABLE `proses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `surat`
 --
 ALTER TABLE `surat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -217,13 +208,6 @@ ALTER TABLE `user`
 ALTER TABLE `disposisi`
   ADD CONSTRAINT `disposisi_ibfk_1` FOREIGN KEY (`surat_id`) REFERENCES `surat` (`id`),
   ADD CONSTRAINT `disposisi_ibfk_2` FOREIGN KEY (`bidang_id`) REFERENCES `bidang` (`id`);
-
---
--- Constraints for table `proses`
---
-ALTER TABLE `proses`
-  ADD CONSTRAINT `proses_ibfk_1` FOREIGN KEY (`surat_id`) REFERENCES `surat` (`id`),
-  ADD CONSTRAINT `proses_ibfk_2` FOREIGN KEY (`bidang_id`) REFERENCES `bidang` (`id`);
 
 --
 -- Constraints for table `surat`
